d0174639ea4c3acd846285f63cdf9917482c221c
shakaaa <67095546+shakaaa@users.noreply.github.com>
2020-08-18 12:51:31 +1000

    Add files via upload
    aquarium java s

---
+++ aquarium.js
@@ -0,0 +1,22 @@
+            window.onload=function() {
+                lengthObj = document.getElementById('txtLength');
+                girthObj = document.getElementById('txtGirth');
+                weightObj = document.getElementById('tdWeight');
+                document.getElementById('btnReset').onclick = resetInputs;
+                document.getElementById('btnCalc').onclick = calcWeight;
+            }
+            function resetInputs() {
+                lengthObj.value = '';
+                girthObj.value = '';
+                weightObj.innerHTML = '';
+            }
+            function calcWeight() {
+                var length = new Number(lengthObj.value);
+                var girth = new Number(girthObj.value);
+                weightObj.innerHTML = '';
+                if(isNaN(length) || isNaN(girth)) {
+                    alert('Invalid length or girth');
+                    return;
+                }
+                weightObj.innerHTML = length*Math.pow(girth,2)/800;
+            }
